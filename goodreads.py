9b10d343cad94d021005405442188eae2d3013c7
linh <linh.ha.nguyen20@gmail.com>
2022-08-30 14:14:30 -0400

    test chromedriver

--- goodreads.py
+++ goodreads.py
@@ -1,28 +1,41 @@
+# TODO:
+#   Write automation for scraping books
+#   Save to dataset (xls/csv)
+
 import requests
 from bs4 import BeautifulSoup as bs
-from selenium import webdriver
 import csv
 import time
 import subprocess as sp
 
-# Todo:
-#   Write automation for scraping books
-#   Save to dataset (xls/csv)
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+def test_driver_manager_chrome():
+    service = ChromeService(executable_path=ChromeDriverManager().install())
+
+    driver = webdriver.Chrome(service=service)
+
+    driver.quit()
+
+test_driver_manager_chrome()
 
 # Scraping
-url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
-page = requests.get(url)
-soup = bs(page.content, 'html.parser')
-book_containers = soup.find_all('tr')
+# url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
+# page = requests.get(url)
+# soup = bs(page.content, 'html.parser')
+# book_containers = soup.find_all('tr')
 
 # Testing open URLs
-base_url = 'https://www.goodreads.com'
-book = book_containers[0]
-book_href = book.find('a', class_='bookTitle')['href']
-book_url = base_url + book_href
-command = ['python3', '-m', 'webbrowser', book_url]
-process = sp.Popen(command, stdout=sp.PIPE)
-output, error = process.communicate()
+# base_url = 'https://www.goodreads.com'
+# book = book_containers[0]
+# book_href = book.find('a', class_='bookTitle')['href']
+# book_url = base_url + book_href
+# command = ['python3', '-m', 'webbrowser', book_url]
+# process = sp.Popen(command, stdout=sp.PIPE)
+# output, error = process.communicate()
+
 
 # data = []
 # for i in range(len(book_containers)):
