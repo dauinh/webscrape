c63373aa1bccefca90922450a3e1ec09c360fe61
linh <linh.ha.nguyen20@gmail.com>
2022-09-04 16:42:13 -0400

    add readme.md

--- goodreads.py
+++ goodreads.py
@@ -11,11 +11,13 @@ import subprocess as sp
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-def script():
+def test_chromedriver():
       driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
       driver.get("https://www.selenium.dev/selenium/web/web-form.html")
       title = driver.title
+      print(title)
       driver.implicitly_wait(0.5)
       text_box = driver.find_element(by=By.NAME, value="my-text")
       submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
@@ -23,13 +25,12 @@ def script():
       submit_button.click()
       # value = message.text
 
-script()
 
 # Scraping
-# url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
-# page = requests.get(url)
-# soup = bs(page.content, 'html.parser')
-# book_containers = soup.find_all('tr')
+url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
+page = requests.get(url)
+soup = bs(page.content, 'html.parser')
+book_containers = soup.find_all('tr')
 
 # Testing open URLs
 # base_url = 'https://www.goodreads.com'
@@ -41,23 +42,27 @@ script()
 # output, error = process.communicate()
 
 
-# data = []
-# for i in range(len(book_containers)):
-#     title = book_containers[i].find('a', class_='bookTitle')
-#     author = book_containers[i].find('a', class_='authorName')
-#     rating = book_containers[i].find('span', class_='minirating')
-#     tmp = rating.get_text().split()
-#     data.append({
-#         'title': title.get_text().strip(),
-#         'author': author.get_text().strip(),
-#         'avg_rating': tmp[0],
-#         'total_rating': tmp[4]
-#     })
+data = []
+for i in range(len(book_containers)):
+    title = book_containers[i].find('a', class_='bookTitle')
+    author = book_containers[i].find('a', class_='authorName')
+    rating = book_containers[i].find('span', class_='minirating')
+    tmp = rating.get_text().split()
+    if len(tmp) > 6:    # special case of 'really liked it' in span
+      tmp = tmp[3:]
+    avg_rating = float(tmp[0])      # convert string to float
+    total_rating = int(tmp[4].replace(',',''))      # convert string to float
+    data.append({
+        'title': title.get_text().strip(),
+        'author': author.get_text().strip(),
+        'avg_rating': avg_rating,
+        'total_rating': total_rating
+    })
 
-# # Saving to dataset
-# with open('goodreads.csv', 'w', encoding='UTF8') as f:
-#     fields = ['title', 'author', 'avg_rating', 'total_rating']
-#     writer = csv.DictWriter(f, fieldnames=fields)
-#     writer.writeheader()
-#     for item in data:
-#         writer.writerow(item)
+# Saving to dataset
+with open('goodreads.csv', 'w', encoding='UTF8') as f:
+    fields = ['title', 'author', 'avg_rating', 'total_rating']
+    writer = csv.DictWriter(f, fieldnames=fields)
+    writer.writeheader()
+    for item in data:
+        writer.writerow(item)
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Webscrape
+
+This project explores how to scrape the web.
+
+## Scrapy
+
+Use the framework [Scrapy](https://docs.scrapy.org/en/latest/intro/tutorial.html) to extract data from [stackoverflow.com](https://stackoverflow.com/questions?pagesize=50&sort=newest) and [indeed.com](https://www.indeed.com/q-software-engineer-jobs.html). However, indeed.com does not allow for bots, so there is no data gathered from this website. 
+
+> **_NOTE:_**  To see which bots are allowed, check for a website robot.txt file.
+
+## BeautifulSoup & Selenium
+
+
+
+## Usage
+
+```python
+import foobar
+
+# returns 'words'
+foobar.pluralize('word')
+
+# returns 'geese'
+foobar.pluralize('goose')
+
+# returns 'phenomenon'
+foobar.singularize('phenomena')
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
