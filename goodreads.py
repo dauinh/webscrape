4f21a365e805194dcd95b495610ca4f64298e905
linh <linh.ha.nguyen20@gmail.com>
2022-09-04 20:20:12 -0400

    extract ratings 1.0

--- goodreads.py
+++ goodreads.py
@@ -27,6 +27,16 @@ def get_book(url):
     title = driver.title
     print(title)
     driver.implicitly_wait(0.5)
+    stars = driver.find_elements(By.CSS_SELECTOR, '[aria-label="Number of ratings and percentage of total ratings"]')
+    
+    ratings = []
+    for i in range(len(stars)):
+        text = stars[i].text
+        text = int(text.split()[0].replace(',',''))
+        ratings.append(text)
+
+    return ratings
+    
 
 # Scraping
 url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
@@ -34,37 +44,37 @@ page = requests.get(url)
 soup = bs(page.content, 'html.parser')
 book_containers = soup.find_all('tr')
 
-# Testing open URLs
+# Opening URLs
 base_url = 'https://www.goodreads.com'
-book_href = book_containers[0].find('a', class_='bookTitle')['href']
-book_url = base_url + book_href
-get_book(book_url)
-# command = ['python3', '-m', 'webbrowser', book_url]
-# process = sp.Popen(command, stdout=sp.PIPE)
-# output, error = process.communicate()
+# for i in range(len(book_containers)):
+for i in range(2):
+    book_href = book_containers[i].find('a', class_='bookTitle')['href']
+    book_url = base_url + book_href
+    ratings = get_book(book_url)
+    print(ratings)
 
 
-data = []
-for i in range(len(book_containers)):
-    title = book_containers[i].find('a', class_='bookTitle')
-    author = book_containers[i].find('a', class_='authorName')
-    rating = book_containers[i].find('span', class_='minirating')
-    tmp = rating.get_text().split()
-    if len(tmp) > 6:    # special case of 'really liked it' in span
-      tmp = tmp[3:]
-    avg_rating = float(tmp[0])      # convert string to float
-    total_rating = int(tmp[4].replace(',',''))      # convert string to float
-    data.append({
-        'title': title.get_text().strip(),
-        'author': author.get_text().strip(),
-        'avg_rating': avg_rating,
-        'total_rating': total_rating
-    })
+# data = []
+# for i in range(len(book_containers)):
+#     title = book_containers[i].find('a', class_='bookTitle')
+#     author = book_containers[i].find('a', class_='authorName')
+#     rating = book_containers[i].find('span', class_='minirating')
+#     tmp = rating.get_text().split()
+#     if len(tmp) > 6:    # special case of 'really liked it' in span
+#       tmp = tmp[3:]
+#     avg_rating = float(tmp[0])      # convert string to float
+#     total_rating = int(tmp[4].replace(',',''))      # convert string to float
+#     data.append({
+#         'title': title.get_text().strip(),
+#         'author': author.get_text().strip(),
+#         'avg_rating': avg_rating,
+#         'total_rating': total_rating
+#     })
 
 # Saving to dataset
-with open('goodreads.csv', 'w', encoding='UTF8') as f:
-    fields = ['title', 'author', 'avg_rating', 'total_rating']
-    writer = csv.DictWriter(f, fieldnames=fields)
-    writer.writeheader()
-    for item in data:
-        writer.writerow(item)
+# with open('goodreads.csv', 'w', encoding='UTF8') as f:
+#     fields = ['title', 'author', 'avg_rating', 'total_rating']
+#     writer = csv.DictWriter(f, fieldnames=fields)
+#     writer.writeheader()
+#     for item in data:
+#         writer.writerow(item)
